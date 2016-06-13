38591a88c16017837e935e52ffced3edefae758f
Seraph08 <djv213@gmail.com>
2016-06-13 14:43:51 -0400

    this is to make it build outside of using the cmsdk

--- configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
@@ -26,7 +26,7 @@ import com.cyanogenmod.settings.device.utils.Constants;
 import com.cyanogenmod.settings.device.utils.FileUtils;
 import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
 
-import com.android.internal.util.cm.ScreenType;
+import com.cyanogenmod.settings.device.utils.ScreenType;
 
 public class ButtonSettings extends NodePreferenceActivity {
     private static final String KEY_IGNORE_AUTO = "notification_slider_ignore_auto";
